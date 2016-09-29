0a8623abdb334fb980bee7f701e0a5f835fc6edc
James Sherriff <jamessherriff@warwickshire.gov.uk>
2016-09-29 14:44:51 +0100

    Bug fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
     service.client_options.application_name = "Homework Tracker"
     service.authorization = current_user.oauth_token
     channel = Google::Apis::CalendarV3::Channel.new(address: "https://lan-lounge.herokuapp.com/notifications",id: "lan-lounge-channel", type: "web_hook")
-    webhook = service.watch_event('primary', channel, single_events: true, time_min: Time.now.iso8601)_to @homework
+    webhook = service.watch_event('primary', channel, single_events: true, time_min: Time.now.iso8601)
   end
 end
