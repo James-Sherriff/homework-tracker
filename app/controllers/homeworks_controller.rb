3cc1880292ac0cf0d1e2a48008c5c7162d337a7d
James Sherriff <jamessherriff@warwickshire.gov.uk>
2016-09-30 08:27:17 +0100

    Changes

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -55,9 +55,7 @@ class HomeworksController < ApplicationController
     @homework = Homework.new
     @homework.title = "From noficiation"
     @homework.content = "dadwadafsfadwadad"
-    puts "Headers inspected - " + env.request.headers.to_s
-    puts "Headers - " + request.headers.env.fetch("X-Goog-Channel-ID")
-    @homework.user = request.headers.env.fetch("X-Goog-Channel-ID")
+    @homework.user = params[:X-Goog-Channel-ID]
     @homework.save
     redirect_to homeworks_path
   end
